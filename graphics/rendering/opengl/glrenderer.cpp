351ab2d04ac1456271f1ebdebcada91918a639e9
Kadeska <kadeska23@gmail.com>
2025-05-13 17:53:55 -0600

    Rectangle now changes color over time.

--- graphics/rendering/opengl/glrenderer.cpp
+++ graphics/rendering/opengl/glrenderer.cpp
@@ -1,11 +1,19 @@
 #include "glrenderer.hpp"
 #include "program.hpp"
 #include <GLFW/glfw3.h>
+#include <cmath>
 
 void renderWindow(GLFWwindow *window)
 {
     // Generate shader program and get VAO/VBO handles
     RenderData renderData = genShaderProgram();
+
+    // // -----------------------------------------------------------------------------
+    // // checking GL_MAX_VERTEX_ATTRIBS
+    // int nrAttributes;
+    // glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+    // std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes;
+    // // -----------------------------------------------------------------------------
 
     while (!glfwWindowShouldClose(window))
     {
@@ -16,9 +24,15 @@ void renderWindow(GLFWwindow *window)
 
         // Use our shader program and bind VAO
         glUseProgram(renderData.shaderProgram);
+        // update the uniform color
+        float timeValue = glfwGetTime();
+        float greenValue = sin(timeValue) / 2.0f + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(renderData.shaderProgram, "ourColor");
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
         glBindVertexArray(renderData.VAO);
 
         // Draw the rectangle (two triangles)
+        // glDrawElements(GL_POINTS, 6, GL_UNSIGNED_INT, 0);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
--- include_headers/shadercode.hpp
+++ include_headers/shadercode.hpp
@@ -2,16 +2,14 @@
 
 const char *vertexShaderSource = "#version 330 core\n"
                                  "layout (location = 0) in vec3 aPos;\n"
-                                 "out vec4 vertexColor;\n"
                                  "void main()\n"
                                  "{\n"
                                  "gl_Position = vec4(aPos, 1.0);\n"
-                                 "vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
                                  "}\0";
 const char *fragmentShaderSource = "#version 330 core\n"
                                    "out vec4 FragColor;\n"
-                                   "in vec4 vertexColor;\n"
+                                   "uniform vec4 ourColor;\n"
                                    "void main()\n"
                                    "{\n"
-                                   "   FragColor = vertexColor;\n"
+                                   "   FragColor = ourColor;\n"
                                    "}\n\0";
